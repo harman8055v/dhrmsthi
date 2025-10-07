2b18ae6a18029f5ec6ccab20636b4ee86fb6222a
harman8055v <harmanbatish11@gmail.com>
2025-10-07 17:17:55 +0530

    chore: push again

--- app/login-assistance/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginAssistanceClient from "./LoginAssistanceClient"
-
-export const dynamic = "force-dynamic"
-
-export default function LoginAssistancePage() {
-  return <LoginAssistanceClient />
-}
-
-
